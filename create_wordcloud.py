864aeb947ea586f8d55289b6f98f953847414730
Amirmohseni87 <amirmohseni87@yahoo.com>
2022-09-05 12:58:43 +0430

    added wordcloud representation

--- create_wordcloud.py
+++ create_wordcloud.py
@@ -13,8 +13,8 @@ def get_words(address):
 
 text = get_words(address="speech.txt")
 
-wordcloud = WordCloud().generate(text)
-
-plt.imshow(wordcloud, interpolation='bilinear')
+wordcloud = WordCloud(max_font_size=50, max_words=100, background_color="white").generate(text)
+plt.figure()
+plt.imshow(wordcloud, interpolation="bilinear")
 plt.axis("off")
 plt.show()
