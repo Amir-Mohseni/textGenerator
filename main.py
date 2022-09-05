a585c6e71f056e380dab574a839ea8ccdc51b3e2
Amirmohseni87 <amirmohseni87@yahoo.com>
2022-09-05 12:30:34 +0430

    made the processing more efficient by using graphs

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ import random
 address = "speech.txt"
 words = fileToString.get_words(address)
 
+
+class Node:
+    def __init__(self, wrd):
+        self.start = self.end = False
+        self.word = wrd
+        self.next = []
+        self.probability = []
+
+    def add_next(self, next_node):
+        self.next.append(next_node)
+
+
 next_options = dict()
 all_words = set()
 first_words = set()
@@ -30,33 +42,48 @@ for i in range(len(words)):
             next_options[word] = [next_word]
 all_words = list(all_words)
 first_words = list(first_words)
+node_list = []
+
+
+def create_graph():
+    for wrd in all_words:
+        node = Node(wrd)
+        if wrd in first_words:
+            node.start = True
+        if wrd in last_words:
+            node.end = True
+        node_list.append(node)
+        word_list = []
+        occurrences = dict()
+        total = 0
+        for item in next_options[node.word]:
+            if item in occurrences:
+                occurrences[item] += 1
+            else:
+                occurrences[item] = 1
+            total += 1
+        for (key, value) in occurrences.items():
+            word_list.append((value, key))
+        word_list.sort()
+        p = 0
+        for item in word_list:
+            cnt = item[0]
+            item_probability = cnt / total
+            node.probability.append((p, p + item_probability))
+            node.next.append(item[1])
+            p += item_probability
+
+
+create_graph()
 
 
 def get_next_word(cur_word):
-    word_list = []
-    occurrences = dict()
-    total = 0
-    for item in next_options[cur_word]:
-        if item in occurrences:
-            occurrences[item] += 1
-        else:
-            occurrences[item] = 1
-        total += 1
-    for (key, value) in occurrences.items():
-        word_list.append((value, key))
-    word_list.sort()
-    p = 0
-    probability = []
-    for item in word_list:
-        cnt = item[0]
-        item_probability = cnt / total
-        probability.append((p, p + item_probability))
-        p += item_probability
+    node = node_list[all_words.index(cur_word)]
     rand_num = random.uniform(0, 1)
-    for i in range(len(probability)):
-        if rand_num >= probability[i][0] and rand_num <= probability[i][1]:
-            return word_list[i][1]
-    return word_list[-1][1]
+    for i in range(len(node.probability)):
+        if node.probability[i][0] <= rand_num <= node.probability[i][1]:
+            return node.next[i]
+    return node.next[-1]
 
 
 def get_random_sentence():
